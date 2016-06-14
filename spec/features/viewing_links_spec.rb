a03282b8d5484d823e480f927e2d7729e62e8991
Stephen Dawes <sdawes@outlook.com>
2016-06-14 10:01:20 +0100

    add feature test and link model

---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,10 @@
+feature 'Viewing Links' do
+  scenario 'so I can see a list of links' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+  end  
+end
