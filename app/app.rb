cb10e8f7d97b8b9a4eecbbffe0534e8e28a6d60c
joestephens <joe.stephens@live.co.uk>
2016-06-14 16:52:45 +0100

    Added multiple tags

--- app/app.rb
+++ app/app.rb
@@ -15,8 +15,11 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.create(title: params[:title], url: params[:url])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+
+    params[:tags].split.each do |tag|
+      link.tags << Tag.first_or_create(name: tag)
+    end
+
     link.save
     redirect to '/links'
   end
@@ -26,11 +29,6 @@ class BookmarkManager < Sinatra::Base
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
-      # if tag
-      #   @links = tag.links
-      # else
-      #   @links = []
-      # end
     erb :'links/index'
   end
 
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -3,12 +3,13 @@ feature 'Adding tags' do
     visit '/links/new'
     fill_in(:title, with: "theguardian")
     fill_in(:url, with: "http://www.theguardian.com")
-    fill_in(:tags, with: "news")
+    fill_in(:tags, with: "news leftwing")
 
     click_button('Add Link')
 
     link = Link.first
-    expect(link.tags.map(&:name)).to include('news')
+    # get the tags that link has and use map so only returns tag names (ignore ids). See if news is in tag names.
+    expect(link.tags.map(&:name)).to include('news', 'leftwing')
     # expect(link.tags.map { |x| x.name }).to include('news')
   end
 end
