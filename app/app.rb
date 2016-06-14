2f897384cbc4e245452724cc6cafee39c979f816
Stephen Dawes <sdawes@outlook.com>
2016-06-14 16:16:41 +0100

    add tag filter feature test and model code

--- app/app.rb
+++ app/app.rb
@@ -21,5 +21,18 @@ class BookmarkManager < Sinatra::Base
     redirect to '/links'
   end
 
+#:name is irrelevant, it is just so you can type in what you want in the url
+# same with the second name request, it is just reffering to what is in the url.
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+      # if tag
+      #   @links = tag.links
+      # else
+      #   @links = []
+      # end
+    erb :'links/index'
+  end
+
   run! if app_file == $0
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,10 +1,24 @@
 feature 'Viewing Links' do
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.theguardian.com', title: 'theguardian', tags: [Tag.first_or_create(name: 'news')])
+  end
+
   scenario 'so I can see a list of links' do
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
-  end  
+  end
+
+  scenario 'so I can filter links by tags' do
+    visit '/tags/news'
+    expect(page.status_code).to eq(200)
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).to have_content('theguardian')
+    end
+
+  end
 end
