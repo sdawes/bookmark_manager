600a3a9aedcd6c4feb8bc87e2184fb51dca9c478
Stephen Dawes <sdawes@outlook.com>
2016-06-16 14:18:45 +0100

    add email format and validation

--- models/user.rb
+++ models/user.rb
@@ -7,9 +7,11 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
   validates_confirmation_of :password
+  # validates_presence_of :emails
+  validates_format_of :email, as: :email_address
 
   property :id, Serial
-  property :email, String
+  property :email, String, format: :email_address, required: true
   property :password_digest, Text, length: 60
 
   def password=(password)
--- spec/features/adding_user_spec.rb
+++ spec/features/adding_user_spec.rb
@@ -12,7 +12,15 @@ feature 'Singing in a user' do
   scenario 'shows error message if passwords don\'t match' do
     sign_in(password_confirmation: 'incorrect')
     expect(page.current_path).to eq '/users'
-    expect(page).to have_content 'Your passwords do not match, please try again.' 
+    expect(page).to have_content 'Your passwords do not match, please try again.'
 
+  end
+
+  scenario 'doesnt create user if email is blank' do
+    expect{ sign_in(email: '') }.not_to change(User, :count)
+  end
+
+  scenario 'has a valid email address' do
+    expect{ sign_in(email: 'freddy.gmail.com') }.not_to change(User, :count)
   end
 end
